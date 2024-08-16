dd41c989cd23a2c411449a7504a40578d1ed32aa
egarcia0992 <78242213+egarcia0992@users.noreply.github.com>
2024-08-16 00:26:42 -0500

    Home Update

--- imanager/src/components/Home.jsx
+++ imanager/src/components/Home.jsx
@@ -1,9 +1,95 @@
 import React from "react";
+import { Table } from "react-bootstrap";
 
 function Home() {
     return (
         <>
             <h1>Home</h1>
+            <div style={{ display: "flex" }} className="container">
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th scope="col">Customer ID</th>
+                            <th scope="col">All Customers</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Date Created</th>
+                            <th scope="col">Update</th>
+                            <th scope="col">Delete</th>
+                        </tr>
+                    </thead>
+
+                    {/* <tbody>
+                        {customers.map((c, index) => (
+                            <tr key={index}>
+                                <td>{c.customerId}</td>
+                                <td>{c.customerName}</td>
+                                <td>{c.customerEmail}</td>
+                                <td>{formatDateTime(c.createdAt)}</td>
+                                <td>
+                                    <Button
+                                        onClick={() =>
+                                            navigate("/products")
+                                        }
+                                    >
+                                        Update
+                                    </Button>
+                                </td>
+                                <td>
+                                    <Button
+                                        onClick={() =>
+                                            navigate("/customers")
+                                        }
+                                    >
+                                        Delete
+                                    </Button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody> */}
+                </Table>
+
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th scope="col">Customer ID</th>
+                            <th scope="col">All Customers</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Date Created</th>
+                            <th scope="col">Update</th>
+                            <th scope="col">Delete</th>
+                        </tr>
+                    </thead>
+
+                    {/* <tbody>
+                        {customers.map((c, index) => (
+                            <tr key={index}>
+                                <td>{c.customerId}</td>
+                                <td>{c.customerName}</td>
+                                <td>{c.customerEmail}</td>
+                                <td>{formatDateTime(c.createdAt)}</td>
+                                <td>
+                                    <Button
+                                        onClick={() =>
+                                            navigate("/products")
+                                        }
+                                    >
+                                        Update
+                                    </Button>
+                                </td>
+                                <td>
+                                    <Button
+                                        onClick={() =>
+                                            navigate("/customers")
+                                        }
+                                    >
+                                        Delete
+                                    </Button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody> */}
+                </Table>
+            </div>
         </>
     );
 }
